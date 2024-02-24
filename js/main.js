d723abe83a808bee684fb356d16ddd85bb4e3136
Akash Saini <akashsaini0020@gmail.com>
2024-02-25 00:10:47 +0530

    header sticky done

---
+++ js/main.js
@@ -0,0 +1,53 @@
+// ============ active header ==============
+let nav = document.querySelector('.navigation-wrap');
+// window.onscroll = function(){
+//     if(document.documentElement.scrollTop > 20){
+//         nav.classList.add("active")
+//     }
+// }
+
+window.addEventListener("scroll",()=>{
+    const scroll = 20;
+    if(window.scrollY >= scroll){
+        nav.classList.add('active');
+    }else{
+        nav.classList.remove('active');
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ============ counter section ============
+
+document.addEventListener('DOMContentLoaded',()=>{
+    function counter(id,start,end,duration){
+        let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration / range)),
+        timer = setInterval(()=>{
+            current += increment;
+            obj.textContent = current;
+            if(current === end){
+                clearInterval(timer);
+            }
+        },step);
+    }
+    counter("count1", 0, 1287, 1000);
+    counter("count2", 0, 3787, 1000);
+    counter("count3", 0, 2769, 1000);
+    counter("count4", 0, 3487, 1000);
+})
