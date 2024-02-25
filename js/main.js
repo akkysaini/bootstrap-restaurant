58a62c313b8c4ed559092ab72062a25e05899cd4
Akash Saini <akashsaini0020@gmail.com>
2024-02-25 11:54:51 +0530

    about section added

--- js/main.js
+++ js/main.js
@@ -1,53 +1,50 @@
 // ============ active header ==============
-let nav = document.querySelector('.navigation-wrap');
+let nav = document.querySelector(".navigation-wrap");
 // window.onscroll = function(){
 //     if(document.documentElement.scrollTop > 20){
 //         nav.classList.add("active")
-//     }
+//     }else{
+//   nav.classList.remove('active');
+// }
 // }
 
-window.addEventListener("scroll",()=>{
-    const scroll = 20;
-    if(window.scrollY >= scroll){
-        nav.classList.add('active');
-    }else{
-        nav.classList.remove('active');
-    }
-})
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 40) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
+});
 
+// nav hide
 
-
-
-
-
-
-
-
-
-
-
-
-
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+  a.addEventListener("click", () => {
+    navCollapse.classList.remove("show");
+  });
+});
 
 // ============ counter section ============
 
-document.addEventListener('DOMContentLoaded',()=>{
-    function counter(id,start,end,duration){
-        let obj = document.getElementById(id),
-        current = start,
-        range = end - start,
-        increment = end > start ? 1 : -1,
-        step = Math.abs(Math.floor(duration / range)),
-        timer = setInterval(()=>{
-            current += increment;
-            obj.textContent = current;
-            if(current === end){
-                clearInterval(timer);
-            }
-        },step);
-    }
-    counter("count1", 0, 1287, 1000);
-    counter("count2", 0, 3787, 1000);
-    counter("count3", 0, 2769, 1000);
-    counter("count4", 0, 3487, 1000);
-})
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+    let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+        current += increment;
+        obj.textContent = current;
+        if (current === end) {
+          clearInterval(timer);
+        }
+      }, step);
+  }
+  counter("count1", 0, 1287, 1000);
+  counter("count2", 0, 3787, 1000);
+  counter("count3", 0, 2769, 1000);
+  counter("count4", 0, 3487, 1000);
+});
